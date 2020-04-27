3f1d3d2624dd5c99ade6dbdbf7a5c1f9f1e98f2d
William Campbell <campbellwilliam94@gmail.com>
2020-04-26 23:29:13 -0400

    Initial Commit

---
+++ routes/auth.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const User = require("../model/User");
+
+//Validation
+const Joi = require("@hapi/joi");
+
+const schema = Joi.object({
+  name: Joi.string().min(6).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(6).required(),
+});
+
+router.post("/register", async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// router.post('/login') test
+
+module.exports = router;
